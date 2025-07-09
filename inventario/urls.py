54c0245c82ae1fe3d64e8fb7f6df099072a13138
yeison <yeison@yeisontech.com>
2025-07-09 09:33:18 -0500

    Crud terminado para el Modelo Categoria

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from inventario.views import CategoriaView, CategoriaNew, CategoriaEdit
+from inventario.views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDelete
 
 app_name = "inventario"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("categorias/", CategoriaView.as_view(), name="categorias"),
     path("categoria/new", CategoriaNew.as_view(),name="categoria-new"),
     path("categoria/edit/<int:pk>", CategoriaEdit.as_view(), name="categoria-edit"),
+    path("categoria/delete/<int:pk>", CategoriaDelete.as_view(), name="categoria-delete"),
 ]
